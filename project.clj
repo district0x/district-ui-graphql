71249fb708774dfa989786b8998144d09d57dc0a
madvas <matus.lestan@gmail.com>
2018-04-06 11:40:25 -0700

    vbump

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject district0x/district-ui-graphql "1.0.0"
+(defproject district0x/district-ui-graphql "1.0.1"
   :description "district UI module for GraphQL integration"
   :url "https://github.com/district0x/district-ui-graphql"
   :license {:name "Eclipse Public License"
