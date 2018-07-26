d22491aac734bc8932e41cdd4bc7f85d460d817d
Juan Monetta <jpmonettas@gmail.com>
2018-07-26 10:05:04 -0300

    Fixing typename middleware so it doesn't add an item into a empty collection

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject district0x/district-ui-graphql "1.0.5"
+(defproject district0x/district-ui-graphql "1.0.6"
   :description "district UI module for GraphQL integration"
   :url "https://github.com/district0x/district-ui-graphql"
   :license {:name "Eclipse Public License"
