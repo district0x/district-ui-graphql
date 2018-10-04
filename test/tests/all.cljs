37fa96bccc50691999891a59b6c7f0a35a127abb
Dom Kiva-Meyer <git@domkm.com>
2018-10-04 18:08:01 -0400

    Add BigNumber type tests

--- test/tests/all.cljs
+++ test/tests/all.cljs
@@ -1,16 +1,17 @@
 (ns tests.all
   (:require
-    [cljs-time.core :as t]
-    [cljs.test :refer [deftest is testing run-tests async use-fixtures]]
-    [day8.re-frame.test :refer [run-test-async run-test-sync wait-for]]
-    [district.graphql-utils :as graphql-utils]
-    [district.ui.graphql.events :as events]
-    [district.ui.graphql.middleware.resolver :refer [create-resolver-middleware]]
-    [district.ui.graphql.subs :as subs]
-    [district.ui.graphql]
-    [mount.core :as mount]
-    [re-frame.core :refer [reg-fx reg-event-fx subscribe reg-cofx reg-sub dispatch trim-v]]
-    [re-frame.db :refer [app-db]]))
+   [bignumber.core :as bn]
+   [cljs-time.core :as t]
+   [cljs.test :refer [deftest is testing run-tests async use-fixtures]]
+   [day8.re-frame.test :refer [run-test-async run-test-sync wait-for]]
+   [district.graphql-utils :as graphql-utils]
+   [district.ui.graphql.events :as events]
+   [district.ui.graphql.middleware.resolver :refer [create-resolver-middleware]]
+   [district.ui.graphql.subs :as subs]
+   [district.ui.graphql]
+   [mount.core :as mount]
+   [re-frame.core :refer [reg-fx reg-event-fx subscribe reg-cofx reg-sub dispatch trim-v]]
+   [re-frame.db :refer [app-db]]))
 
 
 (def gql-sync (aget js/GraphQL "graphqlSync"))
@@ -21,9 +22,10 @@
 (def schema "
   scalar Date
   scalar Keyword
+  scalar BigNumber
 
   type Query {
-    searchUsers(user_registeredAfter: Date, some_param: String): UserList
+    searchUsers(user_registeredAfter: Date, some_param: String, user_age: BigNumber): UserList
     params(db: String!, keys: [Keyword!]): [Parameter]
   }
 
@@ -37,6 +39,7 @@
     user_address: String
     user_status: Keyword
     user_registeredOn: Date
+    user_age: BigNumber
     user_active_: Boolean
     user_favoriteNumbers: [Int]
     user_params(param_otherKey: String): [Parameter]
@@ -49,7 +52,7 @@
     param_otherKey: String
     param_creator: User
   }
-")
+  ")
 
 (def response-root-value (atom nil))
 (def response-schema (atom schema))
@@ -64,15 +67,15 @@
       (let [{:strs [query variables]} (js->clj (js/JSON.parse (aget req "body")))
             res (gql-sync (-> (build-schema @response-schema)
                             (graphql-utils/add-keyword-type)
-                            (graphql-utils/add-date-type))
-                          query
-                          (graphql-utils/clj->js-root-value @response-root-value)
-                          {}
-                          (clj->js variables)
-                          nil)]
+                            (graphql-utils/add-date-type)
+                            (graphql-utils/add-bignumber-type))
+                  query
+                  (graphql-utils/clj->js-root-value @response-root-value)
+                  {}
+                  (clj->js variables)
+                  nil)]
         (let [body (js/JSON.stringify res)]
           (resolve (js/Response. (clj->js body) (clj->js {:status 200}))))))))
-
 
 (use-fixtures
   :each
@@ -87,21 +90,23 @@
 
 (def mount-args {:schema schema :url "http://localhost:1234/" :fetch-opts {:customFetch custom-fetch}})
 
-
 (deftest basic-query
   (run-test-async
     (-> (mount/with-args {:graphql mount-args})
       (mount/start))
 
     (testing "Basic query, resolves values, vectors, scalars properly"
-      (set-response! {:search-users (fn [{:keys [:user/registered-after]}]
+      (set-response! {:search-users (fn [{:keys [:user/registered-after :user/age]}]
                                       (when-not (t/equal? registered-after (t/date-time 2018 10 10))
                                         (throw (js/Error. "Pass registered-after is not correct" registered-after)))
+                                      (when-not (bn/= age (bn/number "10e10"))
+                                        (throw (js/Error. "Pass age is not correct" age)))
                                       {:total-count 1
                                        :items (constantly
                                                 [{:user/id 1
                                                   :user/address "Street 1"
                                                   :user/registered-on (t/date-time 2018 05 05)
+                                                  :user/age (bn/number "10e10")
                                                   :user/status :user.status/active
                                                   :user/favorite-numbers [1 2 3]
                                                   :user/active? true
@@ -112,10 +117,12 @@
                                                                    :param/other-key other-key}])}])})})
 
       (let [query1 (subscribe [::subs/query {:queries [[:search-users
-                                                        {:user/registered-after (t/date-time 2018 10 10)}
+                                                        {:user/registered-after (t/date-time 2018 10 10)
+                                                         :user/age (bn/number "10e10")}
                                                         [:total-count
                                                          [:items [:user/address
                                                                   :user/registered-on
+                                                                  :user/age
                                                                   :user/status
                                                                   :user/active?
                                                                   :user/favorite-numbers
@@ -129,11 +136,12 @@
             (wait-for [::events/set-query-loading]
               (is (not (:graphql/loading? @query1)))
               (let [{:keys [:items :total-count]} (:search-users @query1)
-                    {:keys [:user/address :user/registered-on :user/status :user/favorite-numbers :user/params :user/active?]}
+                    {:keys [:user/address :user/registered-on :user/age :user/status :user/favorite-numbers :user/params :user/active?]}
                     (first items)]
                 (is (= 1 (:total-count (:search-users @query1))))
                 (is (= address "Street 1"))
                 (is (t/equal? registered-on (t/date-time 2018 05 05)))
+                (is (bn/= age (bn/number "10e10")))
                 (is (= status :user.status/active))
                 (is (= favorite-numbers [1 2 3]))
                 (is (= params [{:param/db "b", :param/other-key "kek"}]))
@@ -213,14 +221,19 @@
                                {:operation {:operation/type :query
                                             :operation/name :some-other-query}
                                 :queries [[:search-users
-                                           {:some/param :$a :user/registered-after :$date}
+                                           {:some/param :$a
+                                            :user/registered-after :$date
+                                            :user/age :$age}
                                            [[:items [:user/address]]]]]
                                 :variables [{:variable/name :$a
                                              :variable/type :String!}
                                             {:variable/name :$date
-                                             :variable/type :Date}]}
+                                             :variable/type :Date}
+                                            {:variable/name :$age
+                                             :variable/type :BigNumber}]}
                                {:variables {:a "abcd"
-                                            :date (t/date-time 2018 10 10)}}])]
+                                            :date (t/date-time 2018 10 10)
+                                            :age (bn/number "10e10")}}])]
 
         (wait-for [::events/normalize-response]
           (is (nil? (:graphql/errors @query1)))
@@ -340,6 +353,7 @@
                                                      (js/Promise. (fn [resolve]
                                                                     (resolve :user.status/active))))
                                       :user/registered-on (constantly (t/date-time 2018 9 9))
+                                      :user/age (constantly (bn/number "10e10"))
                                       :user/favorite-numbers (fn []
                                                                (js/Promise. (fn [resolve]
                                                                               (resolve [9 8 7]))))
@@ -374,16 +388,17 @@
                                                                                         {:user/id 2
                                                                                          :user/active? false}))))})))))}}]
       (-> (mount/with-args {:graphql (merge mount-args
-                                            {:query-middlewares [(create-resolver-middleware :middleware1 middleware1)
-                                                                 (create-resolver-middleware :middleware2 middleware2)]})})
+                                       {:query-middlewares [(create-resolver-middleware :middleware1 middleware1)
+                                                            (create-resolver-middleware :middleware2 middleware2)]})})
         (mount/start)))
 
-    (set-response! {:search-users (fn [{:keys [:user/registered-after]}]
+    (set-response! {:search-users (fn [{:keys [:user/registered-after :user/age]}]
                                     {:total-count 1
                                      :items (constantly
                                               [{:user/id 1
                                                 :user/address "Street 1"
                                                 :user/registered-on (t/date-time 2018 5 5)
+                                                :user/age (bn/number "10e10")
                                                 :user/status :user.status/blocked
                                                 :user/favorite-numbers [1 2 3]
                                                 :user/active? true
@@ -408,10 +423,12 @@
     (let [query1 (subscribe [::subs/query {:operation {:operation/type :query
                                                        :operation/name :some-query}
                                            :queries [[:search-users
-                                                      {:user/registered-after (t/date-time 2018 10 10)}
+                                                      {:user/registered-after (t/date-time 2018 10 10)
+                                                       :user/age (bn/number "10e10")}
                                                       [:total-count
                                                        [:items [:user/address
                                                                 :user/registered-on
+                                                                :user/age
                                                                 :user/status
                                                                 :user/active?
                                                                 :user/favorite-numbers
@@ -443,11 +460,12 @@
         (is (nil? (:graphql/errors @query2)))
 
         (let [{:keys [:items :total-count]} (:search-users @query1)
-              {:keys [:user/address :user/registered-on :user/status :user/favorite-numbers :user/params :user/active?]}
+              {:keys [:user/address :user/registered-on :user/age :user/status :user/favorite-numbers :user/params :user/active?]}
               (first items)]
           (is (= 2 (:total-count (:search-users @query1))))
           (is (= address "Street 2"))
           (is (t/equal? registered-on (t/date-time 2018 9 9)))
+          (is (bn/= age (bn/number "10e10")))
           (is (= status :user.status/active))
           (is (= favorite-numbers [9 8 7]))
           (is (= params [{:param/db "d", :param/other-key "77" :param/creator {:user/address "Street ABC"}}]))
@@ -482,7 +500,9 @@
 
     (let [query1 (subscribe [::subs/query
                              {:queries [[:search-users
-                                         {:some/param "a" :user/registered-after (t/date-time 2018 10 10)}
+                                         {:some/param "a"
+                                          :user/registered-after (t/date-time 2018 10 10)
+                                          :user/age (bn/number "10e10")}
                                          [:total-count
                                           [:items [:user/address]]]]]}
                              {:id :my-query}])]
@@ -491,7 +511,7 @@
         (is (nil? (:graphql/errors @query1)))
 
         (is (= {:total-count 1 :items [{:user/address "Street 1"}]}
-               (:search-users (first @query1))))
+              (:search-users (first @query1))))
 
         (set-response! {:search-users (fn []
                                         {:total-count 2
@@ -499,23 +519,26 @@
                                                   [{:user/address "Street 2"}])})})
 
         (dispatch [::events/query {:query {:queries [[:search-users
-                                                      {:some/param "b" :user/registered-after (t/date-time 2018 10 10)}
+                                                      {:some/param "b"
+                                                       :user/registered-after (t/date-time 2018 10 10)
+                                                       :user/age (bn/number "10e10")}
                                                       [:total-count
                                                        [:items [:user/address]]]]]}
                                    :id :my-query}])
 
         (wait-for [::events/normalize-response]
           (is (= {:total-count 1 :items [{:user/address "Street 1"}]}
-                 (:search-users (first @query1))))
+                (:search-users (first @query1))))
 
           (is (= {:total-count 2 :items [{:user/address "Street 2"}]}
-                 (:search-users (second @query1)))))))))
+                (:search-users (second @query1)))))))))
 
 
 
 (def readme-tutorial-schema "
    scalar Date
    scalar Keyword
+   scalar BigNumber
 
    type Query {
      user(user_id: ID): User
@@ -526,6 +549,7 @@
      user_id: ID
      user_address: String
      user_registeredOn: Date
+     user_age: BigNumber
      user_premiumMember_: Boolean
      user_cartItems: [CartItem]
    }
@@ -542,12 +566,12 @@
      item_status: Keyword
      item_price: Float
    }
-")
+  ")
 
 (deftest readme-tutorial
   (run-test-async
     (-> (mount/with-args {:graphql (merge mount-args
-                                          {:schema readme-tutorial-schema})})
+                                     {:schema readme-tutorial-schema})})
       (mount/start))
 
     (set-response! {:user (fn []
@@ -555,6 +579,7 @@
                              :user/address "Street 123"
                              :user/status :user.status/active
                              :user/registered-on (t/date-time 2018 10 10)
+                             :user/age (bn/number "10e10")
                              :user/premium-member? true
                              :user/cart-items (fn []
                                                 [{:cart-item/quantity 2
@@ -569,12 +594,13 @@
                                       :item/description "Some Item Description"
                                       :item/status status
                                       :item/price 123.456}])}
-                   readme-tutorial-schema)
+      readme-tutorial-schema)
 
     (let [query1 (subscribe [::subs/query {:queries [[:user
                                                       {:user/id "abc"}
                                                       [:user/address
                                                        :user/registered-on
+                                                       :user/age
                                                        :user/premium-member?
                                                        [:user/cart-items [:cart-item/quantity
                                                                           [:cart-item/item [:item/title
@@ -591,7 +617,7 @@
         (is (nil? (:graphql/errors @query1)))
         (is (nil? (:graphql/errors @query2)))
 
-        (let [{:keys [:user/address :user/registered-on :user/premium-member? :user/cart-items]} (:user @query1)
+        (let [{:keys [:user/address :user/registered-on :user/age :user/premium-member? :user/cart-items]} (:user @query1)
               {:keys [:item/title :item/description :item/status :item/price]}
               (first (:search-items @query2))]
           (is (= address "Street 123"))
@@ -609,10 +635,11 @@
           (is (= "Street 123" (:user/address @(subscribe [::subs/entity :user "abc"]))))
           (is (= "Some Item" (:item/title @(subscribe [::subs/entity :item "xyz"])))))))))
 
+
 (deftest readme-tutorial-empty-items
   (run-test-async
     (-> (mount/with-args {:graphql (merge mount-args
-                                          {:schema readme-tutorial-schema})})
+                                     {:schema readme-tutorial-schema})})
       (mount/start))
 
     (set-response! {:user (fn []
@@ -620,14 +647,16 @@
                              :user/address "Street 123"
                              :user/status :user.status/active
                              :user/registered-on (t/date-time 2018 10 10)
+                             :user/age (bn/number "10e10")
                              :user/premium-member? true
                              :user/cart-items []})}
-                   readme-tutorial-schema)
+      readme-tutorial-schema)
 
     (let [query1 (subscribe [::subs/query {:queries [[:user
                                                       {:user/id "abc"}
                                                       [:user/address
                                                        :user/registered-on
+                                                       :user/age
                                                        :user/premium-member?
                                                        [:user/cart-items [:cart-item/quantity
                                                                           [:cart-item/item [:item/title
@@ -639,5 +668,6 @@
         (let [user (:user @query1)]
           (is (= user {:user/address "Street 123"
                        :user/registered-on (t/date-time 2018 10 10)
+                       :user/age (bn/number "10e10")
                        :user/premium-member? true
                        :user/cart-items []})))))))
